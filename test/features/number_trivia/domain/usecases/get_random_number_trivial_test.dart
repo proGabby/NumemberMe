1dd381f2611c7cf0192098137c9560493727f1f9
proGabby <inimfonwillie@gmail.com>
2022-09-06 12:52:10 +0100

    step2 TTD on get-Random-Number

--- test/features/number_trivia/domain/usecases/get_random_number_trivial_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivial_test.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
+import 'package:number_trivia_app/core/usecases/usecase.dart';
+import 'package:number_trivia_app/features/number_trivia/domain/usecases/get_random_number_trivail.dart';
 import 'get_concrete_number_trivia_test.mocks.dart';
 
 import 'package:number_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
@@ -13,33 +15,32 @@ class MockNumberTriviaRepository extends Mock
 
 @GenerateMocks([MockNumberTriviaRepository])
 void main() {
-  late GetConcreteNumberTrivia usecase;
+  late GetRandomNumberTrivia usecase;
   late MockMockNumberTriviaRepository mockNumberTriviaRepository;
 
   //runs before every individual test
   //where objects should be instantiated
   setUp(() {
     mockNumberTriviaRepository = MockMockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
 
-  final tNumber = 1;
   final tNumberTrivia = NumberTrivia(number: 1, text: 'test');
 
   test(
-    'should get trivia for the number from the repository',
+    'should get trivia from the repository',
     () async {
       // "On the fly" implementation of the Repository using the Mockito package.
       // When getConcreteNumberTrivia is called with any argument, always answer with
       // the Right "side" of Either containing a test NumberTrivia object.
-      when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+      when(mockNumberTriviaRepository.getRandomNumberTrivia())
           .thenAnswer((_) async => Right(tNumberTrivia));
       // The "act" phase of the test. Call the not-yet-existent method.
-      final result = await usecase(Params(number: tNumber));
+      final result = await usecase(NoParams());
       // UseCase should simply return whatever was returned from the Repository
       expect(result, Right(tNumberTrivia));
       // Verify that the method has been called on the Repository
-      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      verify(mockNumberTriviaRepository.getRandomNumberTrivia());
       // Only the above method should be called and nothing more.
       verifyNoMoreInteractions(mockNumberTriviaRepository);
     },
